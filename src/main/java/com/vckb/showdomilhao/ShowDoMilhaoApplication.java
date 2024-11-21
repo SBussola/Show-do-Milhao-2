049e36fdf42d33e586687445b9d4abd4e4e860cc
San <sandokam.bussola@alunos.sc.senac.br>
2024-11-21 20:14:55 -0300

    Commit inicial

---
+++ src/main/java/com/vckb/showdomilhao/ShowDoMilhaoApplication.java
@@ -0,0 +1,13 @@
+package com.vckb.showdomilhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowDoMilhaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowDoMilhaoApplication.class, args);
+	}
+
+}
