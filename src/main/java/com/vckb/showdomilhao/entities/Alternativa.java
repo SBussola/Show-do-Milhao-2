b91206a5f4483cfce9e594ed7b8eeefed1dcd2c0
Augusto Koch <augustok872@gmail.com>
2024-11-27 16:19:40 -0300

    feat(alternativa): Criada a entidade para alternativa

--- src/main/java/com/vckb/showdomilhao/entities/Alternativa.java
+++ src/main/java/com/vckb/showdomilhao/entities/Alternativa.java
@@ -1,10 +1,6 @@
 package com.vckb.showdomilhao.entities;
 
-
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,21 +12,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Pergunta {
+public class Alternativa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String pergunta;
+    private String descricao;
+
+    private Boolean correta;
 
     @ManyToOne
-    @JoinColumn(name = "nivel_id")
-    @JsonBackReference("nivel-pergunta")
-    private Nivel nivel;
-
-    @OneToMany
-    @JsonManagedReference("pergunta-alternativa")
-    private List<Alternativa> alternativas;
+    @JoinColumn(name = "pergunta_id")
+    @JsonBackReference("pergunta-alternativa")
+    private Pergunta pergunta;
 
 }
